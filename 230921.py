3f529662804505625ea4847ebc657ca9c641be57
heyrimz <gpfla4879@gmail.com>
2023-09-21 09:46:49 +0900

    Time_server_실습1

--- 230921.py
+++ 230921.py
@@ -9,9 +9,12 @@ address = ('',5000) # '' = 임의 주소, 포트 번호 = 5000
 s.bind(address) # 소켓과 주소 결합
 s.listen(5) # 연결 대기, 5개 항목까지 수용
 
+client, addr = s.accept() # 연결혀용 , (client socket, rem_addr) 반환
+
 while True:
-    client, addr = s.accept() # 연결혀용 , (client socket, rem_addr) 반환
+    # client, addr = s.accept() # 연결혀용 , (client socket, rem_addr) 반환
     print("Connection requested from", addr)
-    client.send(time.ctime(time.time()).encode()) # 현재 시간을 전송
-    time.sleep(1)
+    if client:
+        time.sleep(1)
+        client.send(time.ctime(time.time()).encode()) # 현재 시간을 전송
     # client.close()
