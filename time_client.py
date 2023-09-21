fdf3f7157f477fdf357d2debabafd8451d82ad58
heyrimz <gpfla4879@gmail.com>
2023-09-21 09:50:06 +0900

    Time_client_실습1

--- time_client.py
+++ time_client.py
@@ -1,11 +1,13 @@
 # time_client.py
 
 import socket # socket 모듈을 불러온다.
+import time
 
 # TCP 소켓 생성
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = ("localhost",5000)
 
+# time.sleep(1)
+sock.connect(address)  # 서버에 연결
 while True:
-    sock.connect(address)  # 서버에 연결
     print("현재시각 : ", sock.recv(1024).decode())  # 수신 내용을 문자열로 변환하여 출력
