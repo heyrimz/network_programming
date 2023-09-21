0678be68eac98332bfb42b2a9beace7a1c7834aa
heyrimz <gpfla4879@gmail.com>
2023-09-21 09:18:16 +0900

    Time_server

--- 230921.py
+++ 230921.py
@@ -1,15 +1,16 @@
 # time_sever.py
 
-import socket
+import socket # socket 모듈 불러온다
 import time
 
+# TCP 소켓 생성
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 address = ('',5000) # '' = 임의 주소, 포트 번호 = 5000
-s.bind(address)
-s.listen(5)
+s.bind(address) # 소켓과 주소 결합
+s.listen(5) # 연결 대기, 5개 항목까지 수용
 
 while True:
-    client, addr = s.accept()
+    client, addr = s.accept() # 연결혀용 , (client socket, rem_addr) 반환
     print("Connection requested from", addr)
-    client.send(time.ctime(time.time()).encode())
+    client.send(time.ctime(time.time()).encode()) # 현재 시간을 전송
     client.close()
