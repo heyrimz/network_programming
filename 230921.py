7ac368a696c650d08382cd44fe4391debb049b35
heyrimz <gpfla4879@gmail.com>
2023-09-21 09:14:52 +0900

    Time_server

--- 230921.py
+++ 230921.py
@@ -2,3 +2,14 @@
 
 import socket
 import time
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+address = ('',5000) # '' = 임의 주소, 포트 번호 = 5000
+s.bind(address)
+s.listen(5)
+
+while True:
+    client, addr = s.accept()
+    print("Connection requested from", addr)
+    client.send(time.ctime(time.time()).encode())
+    client.close()
