7c00ac6ab1733b5bcda03f7bf81e440fa645b43f
heyrimz <gpfla4879@gmail.com>
2023-09-27 17:16:25 +0900

    pyshark

--- pyshark.py
+++ pyshark.py
@@ -7,4 +7,4 @@ print(conf.ifaces)
 # 인터페이스 이름을 실제로 사용하는 넽워크 인터페이스 이름으로 변경하세요.
 
 while True:
-    sniff(ifaces="Software Loopback Interface 1",prn = lambda x:x.show())
+    sniff(iface="Software Loopback Interface 1",prn = lambda x:x.show())
