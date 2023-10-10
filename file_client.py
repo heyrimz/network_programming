7e1fd1ede1b02d89cb784742d3605220d6c2cb97
heyrimz <gpfla4879@gmail.com>
2023-10-10 22:15:55 +0900

    file_client

--- file_client.py
+++ file_client.py
@@ -6,8 +6,10 @@ prot = 2500
 
 # 서버에 연결
 s_sock.connect((host,prot))
-s_sock.send("I am ready".encode())
 
+# 서버에 i am ready
+s_sock.send("I am ready".encode())
+# 서버로 부터 이름 수신
 fn = s_sock.recv(1024).decode() # 받았지만 차이를 위해 새로 지정
 
 with open("./dummy/"+"recv","wb") as f:
