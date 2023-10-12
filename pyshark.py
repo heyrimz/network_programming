82a24d49af540ebdd5856c9f74a3b3f1999f673b
heyrimz <gpfla4879@gmail.com>
2023-10-12 19:35:41 +0900

    pyshark

--- pyshark.py
+++ pyshark.py
@@ -3,5 +3,8 @@ from scapy.all import *
 
 # 패킷 캡처를 시작합니다.
 # 인터페이스 이름을 실제로 사용하는 네트워크 인터페이스 이름으로 변경하세요.
+# print(conf.ifaces)
 while True:
-    sniff(iface="lo0", prn = lambda x:x.show())
+    sniff(iface="Software Loopback Interface 1",prn = lambda x:x.show())
+
+
