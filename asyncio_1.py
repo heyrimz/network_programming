4a7d38989e37a00d883683c03d35bbbb6a6f2eae
heyrimz <gpfla4879@gmail.com>
2023-10-12 09:32:17 +0900

    asyncio_단순 코루틴 정의와 실행

---
+++ asyncio_1.py
@@ -0,0 +1,8 @@
+import asyncio
+
+async def say(what, when):
+    await asyncio.sleep(when)
+    print(what)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(say('hellp world',1))
+loop.close()
