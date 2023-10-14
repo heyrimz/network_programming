c7369c3355a2dce2727eea861cc88d758d94bac3
heyrimz <gpfla4879@gmail.com>
2023-10-14 14:24:51 +0900

    Tkinter_button_event

--- blank_window.py
+++ blank_window.py
@@ -1,14 +1,35 @@
 from tkinter import *
-root = Tk()
 
+count = 0
 
+def count_plus():
+    global count
+    count += 1
+    label.config(text=str(count))
 
-root.title('opt window')                      # 창 이름 설정
-root.geometry('300x200+300+300')              # 창 크키 + 창 좌표 설정
-root.resizable(False,False)       # 창 크기 변경 가능 여부
+def count_minus():
+    global count
+    count -= 1
+    label.config(text=str(count))
 
-label = Label(root, text="Hello TKINTER")    # 레이블 생성
-label.pack()                                 # 레이블을 화면에 배치
+root = Tk()                           # TK인스턴스 생성
+
+label = Label(root,text="0")
+label.pack()
+
+button1 = Button(root, width=10,text='plus',overrelief='solid', command=count_plus)
+button1.pack()
+
+button2 = Button(root, width=10,text='minus',overrelief='solid', command=count_minus)
+button2.pack()
+# 창 띄우기
+# root.title('opt window')                      # 창 이름 설정
+# root.geometry('300x200+300+300')              # 창 크키 + 창 좌표 설정
+# root.resizable(False,False)       # 창 크기 변경 가능 여부
+#
+# # 창에 글씨 쓰기
+# label = Label(root, text="Hello TKINTER")    # 레이블 생성
+# label.pack()                                 # 레이블을 화면에 배치
 
 root.mainloop()                              # 화면 호출
 
