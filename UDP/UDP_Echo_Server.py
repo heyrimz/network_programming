d68b685439a36cc61a20200b9e9f080dbf6f125e
heyrimz <gpfla4879@gmail.com>
2023-10-15 17:00:30 +0900

    udp_echo_server

--- UDP/UDP_Echo_Server.py
+++ UDP/UDP_Echo_Server.py
@@ -1,12 +1,13 @@
 import socket
-
 port = 2500
-BUFSIZE = 1024
+BUFFSIZE = 1024
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(("",port))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("", port))
 
 while True:
-    data, addr = sock.recvfrom(BUFSIZE)
-    print("Received message: ",data.decode())
-    sock.sendto(data,addr)
+	data, addr = sock.recvfrom(BUFFSIZE)
+	print("Received message: ", data.decode())
+
+	resp = input(":")
+	sock.sendto(resp.encode(),addr)
